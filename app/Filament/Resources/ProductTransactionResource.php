2fcb33e7c882ebaffde87f1c357450904da611f9
Kyliaan <rizkymuhamad2321@gmail.com>
2026-01-20 13:43:08 +0700

    ProductTransaction Dan ProdukResource

--- app/Filament/Resources/ProductTransactionResource.php
+++ app/Filament/Resources/ProductTransactionResource.php
@@ -19,31 +19,140 @@ class ProductTransactionResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-shopping-cart';
 
     public static function form(Form $form): Form
-    {
+     {
         return $form
             ->schema([
-                //
+                Forms\Components\Section::make('Customer Information')
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
+
+                        Forms\Components\TextInput::make('phone')
+                            ->required()
+                            ->maxLength(20),
+
+                        Forms\Components\TextInput::make('email')
+                            ->email()
+                            ->required(),
+
+                        Forms\Components\TextInput::make('city')
+                            ->required(),
+
+                        Forms\Components\TextInput::make('post_code')
+                            ->required(),
+
+                        Forms\Components\Textarea::make('address')
+                            ->required(),
+                    ])
+                    ->columns(2),
+
+                Forms\Components\Section::make('Transaction Detail')
+                    ->schema([
+                        Forms\Components\TextInput::make('booking_trx_id')
+                            ->required()
+                            ->unique(ignoreRecord: true),
+
+                        Forms\Components\Select::make('produk_id')
+                            ->relationship('produk', 'name')
+                            ->required(),
+
+                        Forms\Components\TextInput::make('produk_size')
+                            ->required(),
+
+                        Forms\Components\TextInput::make('quantity')
+                            ->numeric()
+                            ->required(),
+
+                        Forms\Components\TextInput::make('sub_total_amount')
+                            ->numeric()
+                            ->prefix('Rp')
+                            ->required(),
+
+                        Forms\Components\TextInput::make('grand_total_amount')
+                            ->numeric()
+                            ->prefix('Rp')
+                            ->required(),
+
+                        Forms\Components\Select::make('promo_code_id')
+                            ->relationship('promoCode', 'code')
+                            ->nullable(),
+
+                        Forms\Components\Toggle::make('is_paid')
+                            ->label('Paid Status')
+                            ->default(false),
+                    ])
+                    ->columns(2),
+
+                Forms\Components\Section::make('Payment Proof')
+                    ->schema([
+                        Forms\Components\FileUpload::make('proof')
+                            ->image()
+                            ->directory('payment-proofs')
+                            ->maxSize(2048)
+                            ->nullable(),
+                    ]),
             ]);
     }
 
     public static function table(Table $table): Table
     {
-        return $table
-            ->columns([
-                //
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
-    }
+    return $table
+        ->columns([
+            Tables\Columns\TextColumn::make('booking_trx_id')
+                ->label('TRX ID')
+                ->searchable(),
+
+            Tables\Columns\TextColumn::make('name')
+                ->label('Customer')
+                ->searchable(),
+
+            Tables\Columns\TextColumn::make('phone')
+                ->label('Phone'),
+
+            Tables\Columns\TextColumn::make('email')
+                ->label('Email'),
+
+            Tables\Columns\TextColumn::make('produk.name')
+                ->label('Product'),
+
+            Tables\Columns\TextColumn::make('produk_size')
+                ->label('Size'),
+
+            Tables\Columns\TextColumn::make('quantity')
+                ->label('Qty'),
+
+            Tables\Columns\TextColumn::make('sub_total_amount')
+                ->label('Sub Total')
+                ->money('IDR'),
+
+            Tables\Columns\TextColumn::make('grand_total_amount')
+                ->label('Grand Total')
+                ->money('IDR'),
+
+            Tables\Columns\ImageColumn::make('proof')
+                ->label('Proof')
+                ->circular(),
+
+            Tables\Columns\IconColumn::make('is_paid')
+                ->label('Paid')
+                ->boolean(),
+
+            Tables\Columns\TextColumn::make('created_at')
+                ->label('Date')
+                ->dateTime('d M Y'),
+        ])
+        ->actions([
+            Tables\Actions\EditAction::make(),
+        ])
+        ->bulkActions([
+            Tables\Actions\BulkActionGroup::make([
+                Tables\Actions\DeleteBulkAction::make(),
+                Tables\Actions\ForceDeleteBulkAction::make(),
+                Tables\Actions\RestoreBulkAction::make(),
+            ]),
+        ]);
+}
 
     public static function getRelations(): array
     {
--- app/Filament/Resources/ProdukResource.php
+++ app/Filament/Resources/ProdukResource.php
@@ -4,89 +4,109 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\ProdukResource\Pages;
 use App\Models\Produk;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
-use Filament\Tables\Table;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Select;
+use Filament\Tables\Actions;
+use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Filters\TrashedFilter;
-use Filament\Tables\Actions;
+use Filament\Tables\Table;
 
 class ProdukResource extends Resource
 {
     protected static ?string $model = Produk::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-shopping-bag';
+
     protected static ?string $navigationLabel = 'Produk';
+
     protected static ?string $pluralModelLabel = 'Produk';
 
     public static function form(Form $form): Form
     {
         return $form->schema([
 
-            Grid::make(2)->schema([
-                TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-
-                TextInput::make('price')
-                    ->numeric()
-                    ->prefix('IDR')
-                    ->required(),
-            ]),
-
-            Grid::make(2)->schema([
-                FileUpload::make('thumbnail')
-                    ->image()
-                    ->directory('produk/thumbnail')
-                    ->required(),
-
-                FileUpload::make('photos')
-                    ->image()
-                    ->multiple()
-                    ->directory('produk/photos'),
-            ]),
-
-            Repeater::make('sizes')
+            Section::make('Products Information')
                 ->schema([
-                    TextInput::make('size')->required(),
-                ])
-                ->columnSpanFull(),
 
-            Textarea::make('about')
-                ->rows(2)
-                ->columnSpanFull(),
+                    Grid::make(2)->schema([
+                        TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
 
-            Select::make('is_popular')
-                ->options([
-                    1 => 'Yes',
-                    0 => 'No',
-                ])
-                ->required(),
+                        TextInput::make('price')
+                            ->numeric()
+                            ->prefix('IDR')
+                            ->required(),
+                    ]),
 
-            Grid::make(3)->schema([
-                Select::make('category_id')
-                    ->relationship('category', 'name')
-                    ->required(),
+                    Grid::make(2)->schema([
+                        FileUpload::make('thumbnail')
+                            ->image()
+                            ->directory('produk/thumbnail')
+                            ->required(),
 
-                Select::make('brand_id')
-                    ->relationship('brand', 'name')
-                    ->required(),
+                        Repeater::make('photos')
+                            ->label('Product Photos')
+                            ->schema([
+                                FileUpload::make('photo')
+                                    ->image()
+                                    ->directory('produk/photos')
+                                    ->required(),
+                            ])
+                            ->addActionLabel('Add Photo'),
+                    ]),
 
-                TextInput::make('stock')
-                    ->numeric()
-                    ->suffix('pcs')
-                    ->required(),
-            ]),
+                    Grid::make(2)->schema([
+                        Repeater::make('sizes')
+                            ->label('Product Sizes')
+                            ->schema([
+                                TextInput::make('size')->required(),
+                            ]),
+                    ]),
 
+                    Section::make('Information Tambahan')
+                        ->schema([
+
+                            Grid::make(2)->schema([
+                                TextInput::make('about')
+                                    ->label('About Product')
+                                    ->required(),
+
+                                Select::make('is_popular')
+                                    ->label('Popular Product')
+                                    ->options([
+                                        1 => 'Yes',
+                                        0 => 'No',
+                                    ])
+                                    ->required(),
+                            ]),
+
+                            Grid::make(3)->schema([
+                                Select::make('category_id')
+                                    ->relationship('category', 'name')
+                                    ->required(),
+
+                                Select::make('brand_id')
+                                    ->relationship('brand', 'name')
+                                    ->required(),
+
+                                TextInput::make('stock')
+                                    ->numeric()
+                                    ->suffix('pcs')
+                                    ->required(),
+                            ]),
+                        ]),
+                ])->columns(1),
         ]);
+
     }
 
     public static function table(Table $table): Table
@@ -122,9 +142,9 @@ class ProdukResource extends Resource
     public static function getPages(): array
     {
         return [
-            'index'  => Pages\ListProduks::route('/'),
+            'index' => Pages\ListProduks::route('/'),
             'create' => Pages\CreateProduk::route('/create'),
-            'edit'   => Pages\EditProduk::route('/{record}/edit'),
+            'edit' => Pages\EditProduk::route('/{record}/edit'),
         ];
     }
 }
